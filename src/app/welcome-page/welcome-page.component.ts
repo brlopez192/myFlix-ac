c8286737ab0bedbed67a417b08638ace0488c4c9
brlopez192 <brlopez192@gmail.com>
2025-02-25 03:55:46 -0500

    welcome page

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common'; // for *ngIf
@@ -10,22 +10,30 @@ import { MovieCardComponent } from '../movie-card/movie-card.component';
 @Component({
   selector: 'app-welcome-page',
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule, CommonModule, MatDialogModule],
+  imports: [RouterOutlet, MatButtonModule, CommonModule],
   templateUrl: './welcome-page.component.html',
   styleUrls: ['./welcome-page.component.scss']
 })
-export class WelcomePageComponent {
-  constructor(public dialog: MatDialog) {}
 
-  openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, { width: '280px' });
+export class WelcomePageComponent {
+
+  constructor(public dialog: MatDialog) { }
+// This is the function that will open the dialog when the signup button is clicked  
+openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+// Assigning the dialog a width
+    width: '280px'
+    });
   }
 
   openUserLoginDialog(): void {
-    this.dialog.open(UserLoginFormComponent, { width: '280px' });
-  }
-
-  openMoviesDialog(): void {
-    this.dialog.open(MovieCardComponent, { width: '500px' });
-  }
+    this.dialog.open(UserLoginFormComponent, {
+        width: "280px"
+    })
+}
+openMoviesDialog(): void {
+  this.dialog.open(MovieCardComponent, {
+    width: '500px'
+  });
+}
 }
