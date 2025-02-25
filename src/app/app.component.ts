30b77384152ca5a1299e5157bf73b5e704dcc911
brlopez192 <brlopez192@gmail.com>
2025-02-25 03:46:59 -0500

    redid routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,13 @@
 import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { MatButtonModule } from '@angular/material/button';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common'; // for *ngIf
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
-import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule, CommonModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
-
-  constructor(public dialog: MatDialog) { }
-// This is the function that will open the dialog when the signup button is clicked  
-openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, {
-// Assigning the dialog a width
-    width: '280px'
-    });
-  }
-
-  openUserLoginDialog(): void {
-    this.dialog.open(UserLoginFormComponent, {
-        width: "280px"
-    })
-}
-openMoviesDialog(): void {
-  this.dialog.open(MovieCardComponent, {
-    width: '500px'
-  });
-}
 }
--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; // for *ngIf
+import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from '../user-login-form/user-login-form.component';
+import { MovieCardComponent } from '../movie-card/movie-card.component';
 
 @Component({
   selector: 'app-welcome-page',
-  imports: [],
+  standalone: true,
+  imports: [RouterOutlet, MatButtonModule, CommonModule, MatDialogModule],
   templateUrl: './welcome-page.component.html',
-  styleUrl: './welcome-page.component.scss'
+  styleUrls: ['./welcome-page.component.scss']
 })
 export class WelcomePageComponent {
+  constructor(public dialog: MatDialog) {}
 
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, { width: '280px' });
+  }
+
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, { width: '280px' });
+  }
+
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, { width: '500px' });
+  }
 }
