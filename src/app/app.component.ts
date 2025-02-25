4c784ecaed04b69c007316439af5b5b54cb8ef82
brlopez192 <brlopez192@gmail.com>
2025-02-24 23:13:10 -0500

    sign up form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; // for *ngIf
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, MatButtonModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'myFlix-ac';
+  title = 'myFlix-Angular-client';
+
+  constructor(public dialog: MatDialog) { }
+// This is the function that will open the dialog when the signup button is clicked  
+openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+// Assigning the dialog a width
+    width: '280px'
+    });
+  }
 }
